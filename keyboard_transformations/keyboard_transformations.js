0051626fb5ad27e17438cb0aae2b51ff61cbb63f
Ganesh Kakade <17514413+ganeshkakade@users.noreply.github.com>
2022-08-18 01:54:19 +0530

    add transformation logic for horizontal and vertical

--- keyboard_transformations/keyboard_transformations.js
+++ keyboard_transformations/keyboard_transformations.js
@@ -84,3 +84,112 @@ It is not necessary that the program take the input from a file or command line 
 Output can be printed to a file or the console.
  
 */
+
+// solution
+
+// define global configuration. hard coded for easiness
+let keyboard = [
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
+]
+
+let trans_keyboard = []
+let txt_indexes = {}
+
+// convert string to string array
+convert_str_to_arr = (str, split_param='') => {
+    return str.replace(/\s+/g, '').split(split_param)
+}
+
+is_transformations_valid = (trans) => {
+    if(Number.isInteger(Number(trans)) || ['H', 'V'].includes(trans)) {
+        return true
+    }
+    return false
+}
+
+horizontal_table_flip = () => {
+    trans_keyboard = trans_keyboard.map(row => row.reverse())
+}
+
+vertical_table_flip = () => {
+    trans_keyboard = trans_keyboard[0].map((_, colIndex) => trans_keyboard.map(row => row[colIndex]));
+    horizontal_table_flip()
+    trans_keyboard = trans_keyboard[0].map((_, colIndex) => trans_keyboard.map(row => row[colIndex]));
+}
+
+table_shift = () => {
+
+}
+
+execute_trans = (trans) => {
+    if(is_transformations_valid(trans)) {
+        switch(trans) {
+            case 'H':
+                horizontal_table_flip()
+                break
+            case 'V':
+                vertical_table_flip()
+                break
+            case trans:
+                table_shift()
+                break
+            default: break
+        }
+    }
+}
+
+// iterate trans arr to perform transformation in a sequence
+process_trans_arr = (trans_arr) => {
+    trans_arr.forEach((trans, i) => {
+        execute_trans(trans)
+    })
+}
+
+// iterate txt arr to find the new str in tranformed keyboard
+process_text_arr = (txt_arr) => {
+    let output = '' // to print output on single line
+    txt_arr.forEach((txt, i) => {
+        // check if txt index record exists
+        if(txt in txt_indexes) {
+            // retrive index for txt from txt indexes
+            output += trans_keyboard[txt_indexes[txt][0]][txt_indexes[txt][1]]
+        } else {
+            // get index of txt from keyboard
+            let row = keyboard.findIndex(row => row.includes(txt))
+            let col = keyboard[row].indexOf(txt)
+
+            // for the same index, output new txt from transformed keyboard
+            output += trans_keyboard[row][col]
+            txt_indexes[txt] = [row, col]
+        }
+    })
+    console.log(output)
+}
+
+// main 
+keyboard_transformations = () => {
+
+    // inputs hardcoded 
+    let input_trans_str = "H,V"
+    let input_txt_str = "qw"
+
+    // deep clone of original keyboard to avoid transformation changes
+    trans_keyboard = JSON.parse(JSON.stringify(keyboard))
+
+    // create string array from input string
+    let trans_arr = convert_str_to_arr(input_trans_str, ',')
+    let txt_arr = convert_str_to_arr(input_txt_str)
+
+    console.table('original keyboard', keyboard)
+
+    process_trans_arr(trans_arr)
+    process_text_arr(txt_arr)
+    
+    console.table('transformed keyboard', trans_keyboard)
+}
+
+// execute the main
+keyboard_transformations()
